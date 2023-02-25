7eda76d1ee24921d92cc9aac96b615cb9910c5ac
kalogira <kalogira@SLS-528Z3S3>
2023-02-25 18:16:07 +0000

    commit

---
+++ ArtGallery/Source/ArtGallery/Drawing.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Drawing.h"
+
+Drawing::Drawing()
+{
+}
+
+Drawing::~Drawing()
+{
+}
---
+++ ArtGallery/Source/ArtGallery/Drawing.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ARTGALLERY_API Drawing
+{
+public:
+	Drawing();
+	~Drawing();
+};
